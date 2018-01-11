d3dd32230675407f73dc19784d8be9e3dc373e20
Sheanoy Encarnacion <sheanoyencarnacion@msn.com>
2018-01-11 04:19:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function whisper(string) {
 }
 function logshout(string) {
   console.log(string.toUpperCase())}
-  
+ function logWhisper(string){ 
+   console.log(string.toLowerCase()) } 
+   function sayHiToGrandma(string){ 
+     if (string.toLowerCase() === string){ return "I can't hear you!" } else if (string.toUpperCase() === string){ return "YES INDEED!" } else if (string === 'I love you, Grandma.') { return 'I love you, too.' } } 
   
